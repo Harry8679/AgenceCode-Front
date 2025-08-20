f8e06b68f96c1cde0f62d575ca5927ef1187ca89
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-20 02:35:15 +0200

    Transition to

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { Link, NavLink } from 'react-router-dom';
+
+const cn = (...a) => {
+    return a.filter(Boolean).join('');
+}
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const linkBase = "block px-3 py-2 rounded-lg text-sm font-medium transition";
+
   return (
-    <div>Navbar</div>
+    <h1>Test</h1>
   )
 }
 export default Navbar;
