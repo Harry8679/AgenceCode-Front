c56526bc28b021323501de4d30c9f6d39d7b2a93
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-20 10:17:16 +0200

    Navbar imported

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/inscription' element={<Register />} />
         <Route path='/connexion' element={<Login />} />
