440cd6a7ff7e5470e355e6cb0232663a6b1418fe
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-22 14:01:29 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Navbar from './components/Navbar';
+// src/App.js
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/inscription' element={<Register />} />
-        <Route path='/connexion' element={<Login />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Routes publiques (uniquement si NON connecté) */}
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <Home />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/inscription"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/connexion"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+
+          {/* Routes privées (uniquement si connecté) */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Fallback 404 simple (optionnel) */}
+          <Route path="*" element={<div style={{ padding: 24 }}>Page introuvable</div>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
