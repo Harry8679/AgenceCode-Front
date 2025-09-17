fc2f1f8bf2dcde31df412b1095ca3f13113b906e
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-17 05:56:42 +0200

    Testimonials imported 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import ContactProfileForm from "../components/ContactProfileForm";
 import PromoDoubleCard from "../components/PromoDoubleCard";
 import studentCut1 from '../assets/images/student-cut1.jpg';
 import studentCut2 from '../assets/images/student-cut2.jpg';
+import TestimonialsCarousel from "../components/TestimonialsCarousel";
 
 const CLASSES = [
   "6e", "5e", "4e", "3e", "2nde", "1ère", "Terminale",
